4d228bc195da68034d9cf37f6aa39b72f821f74a
khushikri1209 <khushikumari12029@gmail.com>
2025-02-28 14:09:22 +0530

    add features

--- StudentManagementSystem.c
+++ StudentManagementSystem.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define FILENAME "students.dat"
 
-struct Students{
+struct Students
+{
     int id;
     char name[50];
     int age;
     float marks;
-
 };
 void addStudent();
 void displayStudent();
@@ -17,9 +17,11 @@ void searchStudfent();
 void deleteStudent();
 void updateStudent();
 
-int main(){
+int main()
+{
     int choice;
-    while(1){
+    while (1)
+    {
         printf("\nStudent Management System\n");
         printf("1. add Student\n");
         printf("2. Display Students\n");
@@ -30,19 +32,36 @@ int main(){
         printf("Enter Your choice: ");
         scanf("%d", &choice);
 
-        switch(choice){
-            case 1: addsStudents(); break;
-            case 2: displayStudents(); break;
-            case 3: searchStudents(); break;
-            case 4: deleteStudents(); break;
-            case 5: updateStudents(); break;
-            case 6: exit(0);
-            default: printf("Invalid choice! Please try again.\n");
-            
+        switch (choice)
+        {
+        case 1:
+            addsStudents();
+            break;
+        case 2:
+            displayStudents();
+            break;
+        case 3:
+            searchStudents();
+            break;
+        case 4:
+            deleteStudents();
+            break;
+        case 5:
+            updateStudents();
+            break;
+        case 6:
+            exit(0);
+        default:
+            printf("Invalid choice! Please try again.\n");
         }
-
-
     }
+    return 0;
 }
-
-
+void addStudent(){
+    FILE *fp = fopen(FILENAME,"ab");
+    if(!fp){
+        printf("Erroe opening file\n");
+        return;
+    }
+    
+}
